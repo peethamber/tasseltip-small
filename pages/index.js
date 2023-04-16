09487089d1475b425baf0f739d884b75443275fa
peethamber <40764612+peethamber@users.noreply.github.com>
2023-04-17 00:10:32 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -13,21 +13,23 @@ export default function Home() {
     
     <div className="mt-5">
     
-    <Container flex style={{fontSize:"17px"}}>
+    <Container flex style={{fontSize:"19px"}}>
     <Header3></Header3>
     <img className="d-block w-100" src="/techpic2.jpeg"></img>
     <div className = "mt-5">
     <img className="d-block w-100" src="/metaverse.jpg"></img>
-    <a style={{textDecoration:"none",color:"white",backgroundColor:'black'}} href="/video.html"  target="_blank" >PLAY VIDEO</a>
-    <p style={{backgroundColor:'grey',color:'white'}}>Best Viewed in Landscape on Mobile</p>
+   
+    <a style={{textDecoration:"none",color:"white",backgroundColor:'grey'}} href="/video.html"  target="_blank" >PLAY VIDEO</a>
+    <p style={{color:'grey'}}>Best Viewed in Landscape on Mobile</p>
     
+
     <img className="d-block w-100" src="/vr2.jpg"></img>
-    <a style={{textDecoration:"none",color:"white",backgroundColor:'black'}} href="/onativideo.html"  target="_blank" >PLAY VIDEO </a>
-    <p style={{backgroundColor:'grey',color:'white'}}>Best Viewed in Landscape on Mobile</p>
+    <a style={{textDecoration:"none",color:"white",backgroundColor:'grey'}} href="/onativideo.html"  target="_blank" >PLAY VIDEO </a>
+    <p style={{color:'grey'}}>Best Viewed in Landscape on Mobile</p>
 
     <img className="d-block w-100" src="/gamify.jpg"></img>
-    <a style={{textDecoration:"none",color:"white",backgroundColor:'black'}} href="/gamevideo.html"  target="_blank" >PLAY VIDEO </a>
-    <p style={{backgroundColor:'grey',color:'white'}}>Best Viewed in Portrait on Mobile</p>
+    <a style={{textDecoration:"none",color:"white",backgroundColor:'grey'}} href="/gamevideo.html"  target="_blank" >PLAY VIDEO </a>
+    <p style={{color:'grey'}}>Best Viewed in Portrait on Mobile</p>
     </div>
     <Footer2></Footer2>
             
