e30a3cd4dc13b7f205e3eb3e2c625ca02d807c2b
peethamber <40764612+peethamber@users.noreply.github.com>
2023-09-29 22:58:23 +0530

    Add files via upload

--- pages/ourofferings.js
+++ pages/ourofferings.js
@@ -11,43 +11,13 @@ export default function gamiform() {
 
   return ( 
     
-    <div className="main-div" style={{backgroundColor:'black'}}>
+    <div className="main-div" style={{backgroundColor:'#f0f8ff'}}>
     <Header3></Header3>
-    <Container flex >
-    <Card border="secondary" style={{fontFamily:"Arial",fontSize:"19px",
-    alignItems:'center',backgroundColor:'black',color:'white'}}>
-    <div className="mt-5">
-  
-    <p style={{fontWeight:"bold"}}>
-    Strategy
-    </p>
-    <p>1. Retail Gamification</p>
-    <p>2. DAO(Decentralized Autonomous Organization) Readiness Study</p>
-    <p>3. Business Process Reengineering and Automation</p>
-
-    <p style={{fontWeight:"bold"}}>
-    Business Led Technology Transformation
-    </p>
-    
-    <p>1. Enterprise Architecture</p>
-    <p>2. Decentralized Applications (DApps)</p>
-    <p>3. Metaverse</p>
-    <p>4. Mobility</p>
-    <p>5. Computer Vision</p>
-    <p>6. Neural Networks</p>
-
-    <p style={{fontWeight:"bold"}}>Industry Specialization</p>
-
-    <p>1. Financial Services and Fintech</p>
-    <p>2. Fashion</p>
-    <p>3. Health Care</p>
-    
-   
-   
-    <p>Get in touch at peethvt@tasseltip.com </p>
-    
+    <Container fluid >
+    <div class="mt-5">  
+      <Image src="/Expertise.png" width="100%"></Image>
     </div>
-    </Card>
+
     <Footer2></Footer2>
     </Container>
   
