13a4bdc12c415c43d0e0aef81498a0811f1b8806
peethamber <40764612+peethamber@users.noreply.github.com>
2023-09-29 22:57:09 +0530

    Add files via upload

---
+++ public/Header3.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Container, Button, Alert, Breadcrumb, Card, Form, Row, Col, Navbar, Nav, NavDropdown, Carousel, Image, ListGroup, InputGroup} 
+from 'react-bootstrap';
+
+export default function Header3() {
+ 
+     return (
+        
+        <div>
+    
+            
+    <a href="/"><img src="Tasseltip_edited.png" className="d-block mx-auto" width="276
+    px"/></a>
+            <Navbar  expand="lg" className="navbar navbar-dark bg-dark" style={{fontSize:"23px"}}>
+            <Container fluid>
+            
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+               
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="navbarNav mx-auto " >
+                <NavDropdown title="Virtual Tour" renderMenuOnMount={true} style={{paddingRight:"17px"}}>
+                                      
+                                      <Row>
+                  
+                                      <Col style={{fontSize:"20px"}}>
+                                          <NavDropdown.Item eventKey="Virtual Office" 
+                                          href="https://www.tasseltip.com/webgl-small/index.html" target="_blank">
+                                              The Office</NavDropdown.Item>
+                                          <NavDropdown.Item eventKey="MensFormals" 
+                                          href="https://www.tasseltip.com/webgl-small-meta/index.html" target="_blank">
+                                              TasselLand Metaverse</NavDropdown.Item>
+                                      </Col>
+                                      
+                                      </Row>
+                                       
+                          </NavDropdown>
+                          <NavDropdown title="Capabilities" renderMenuOnMount={true} style={{paddingRight:"17px"}} >
+                          <Row>
+                            
+                            <Col style={{fontSize:"20px"}}>
+                                <NavDropdown.Item eventKey="Services" 
+                                href="./ourofferings">
+                                    Our Expertise</NavDropdown.Item>
+                                
+                            </Col>
+                            
+                            </Row>
+                   
+                          </NavDropdown>
+                         <NavDropdown title="Gamiform" renderMenuOnMount={true} style={{paddingRight:"17px"}} >
+                          <Row>
+                            
+                            <Col style={{fontSize:"20px"}}>
+                                <NavDropdown.Item eventKey="Gamiform" 
+                                href="./whygamiform">
+                                   Gamiformation</NavDropdown.Item>
+                                
+                            </Col>
+                            
+                            </Row>
+                   
+                         
+                                    
+                          </NavDropdown>
+                    <NavDropdown title="The Team" renderMenuOnMount={true}  >
+                                            
+                        <Row>
+
+                        <Col style={{fontSize:"20px"}}>
+                            <NavDropdown.Item eventKey="About Me" 
+                            href="./AboutMe" >
+                                About Me</NavDropdown.Item>
+                           
+                        </Col>
+                        
+                        <Col style={{fontSize:"20px"}}>
+                            <NavDropdown.Item eventKey="Executive Leadership" 
+                            href="./ExecLeadership" >
+                                Executive Leadership</NavDropdown.Item>
+                        
+                        </Col>
+
+                        </Row>
+                      
+                       
+                        </NavDropdown>
+                                        
+                        
+                       
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+            </div>
+        
+    )
+}
+
+    
+
--- public/index.js
+++ public/index.js
@@ -11,67 +11,22 @@ export default function Home() {
   
   return ( 
     
-    <div style={{backgroundColor:"black"}}>
+    <div style={{backgroundColor:"#f0f8ff"}}>
     <Header3></Header3>
     <Container fluid>
     <p></p>
-    <Card  style={{border:"none"}}>
-               
-               <Card.Body>
-                <Card.Text style={{fontSize:"30px"}}>TASSELLAND Launches the VR Showroom in the Metaverse</Card.Text>
-               <div class="mt-5>" align="center">
-               <video style={{width:"auto"}} controls loop poster="/showroom.jpg" >
-               <source src="/VirtualCarShowroom.mp4" type="video/mp4"/>
+   
+             <div>
+               <Image src="/Home.png" width="100%" ></Image>
+               <Image src="/About.png" width="100%" ></Image>
+               <Image src="/Services.png" width="100%" ></Image>
               
-
-               Your browser does not support the video tag.
-              </video> 
-              </div>   
-    </Card.Body>
-    </Card>
-    <p style={{color:"white",font:"arial-narrow",fontSize:"18px"}}>
-    Gamiform Experiences for Online Business
-    </p>
-    <a href="whygamiform"><img className="d-block w-100" src="/gaming2.jpeg"></img></a>
-    <a style={{textDecoration:"none",color:"white",backgroundColor:'blue'}} href="/gamevideo.html">PLAY VIDEO </a>
-    <p style={{color:'grey'}}>Best Viewed in Portrait on Mobile</p>
-
-    <p style={{color:"white",font:"arial-narrow",fontSize:"18px"}}>
-    Mixed Reality in In-Store and Online Shopping
-    </p>
+               <Image src="/Contact.png" width="100%" ></Image>
+               </div>
+                 
     
-    <a href="whygamiform"><img className="d-block w-100" src="/ar.webp" ></img></a>
-
-    <p style={{color:"white",font:"arial-narrow",fontSize:"18px"}}>
-    AI powered Product Recommenders for In-Store and Online Shopping
-    </p>
-    <a href="whygamiform"><img  className="d-block w-100"  src="/fashionreco.jpg"></img></a>
-   
-
-   
-    {/* <a href="whygamiform"><img  className="d-block w-100"  src="/techpic2.jpeg"></img></a> */}
-    
-    <p style={{color:"white",font:"arial-narrow",fontSize:"18px"}}>
-    We are building out our own Metaverse, TasselLand. 
-    </p>
-    <img className="d-block w-100" src="/metaverse.jpg"></img>
-   
-    <a style={{textDecoration:"none",color:"white",backgroundColor:'blue'}} href="/video.html">PLAY VIDEO</a>
-    <p style={{color:'grey'}}>Best Viewed in Landscape on Mobile</p>
-    
-    <p style={{color:"white",font:"arial-narrow",fontSize:"20px"}}>
-    Immersive Experience in Fashion
-    </p>
-    <img className="d-block w-100" src="/vr2.jpg"></img>
-    <a style={{textDecoration:"none",color:"white",backgroundColor:'blue'}} href="/onativideo.html">PLAY VIDEO </a>
-    <p style={{color:'grey'}}>Best Viewed in Landscape on Mobile</p>
-
-   
-    
-    <Footer2></Footer2>
-            
     </Container>
-  
+   
    </div>
   )
 }
