5542a335435b415c1068d1b4110453b997fb434d
peethamber <40764612+peethamber@users.noreply.github.com>
2023-12-25 17:13:22 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,19 @@ export default function Home() {
               <Header3></Header3>
               <p></p>
               <Image src="/Home.png" width="100%" ></Image>
+              <h4 align="center">T A S S E L L A N D - TEASER</h4>
+              <div>{domLoaded?<ReactPlayer
+              url="https://vimeo.com/897717888?share=copy" 
+                
+                playing="true"
+                width="100%"
+                height="50vh"
+                controls="true"
+                />:''}</div>
+                <p></p>
              
-              <h4 align="center">IMMERSIVE TRAINING in TASSELLAND</h4>
+             
+              <h4 align="center">IMMERSIVE TRAINING IN TASSELLAND</h4>
               <div>{domLoaded?<ReactPlayer
               url="https://vimeo.com/897710781?share=copy" 
                 
