6254a701a23c365ed025eb8a5031ab25d309c914
peethamber <40764612+peethamber@users.noreply.github.com>
2024-01-31 00:00:11 +0000

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useRef } from 'react';
 import {Container, Button, Alert, Breadcrumb, Card, Form, Row, Col, Navbar, Nav, NavDropdown, Carousel, Image, ListGroup, InputGroup} 
 from 'react-bootstrap';
@@ -6,34 +6,88 @@ from 'react-bootstrap';
 
 import Header4 from './Header4.js'
 import { useMediaQuery } from 'react-responsive'
+import MediaQuery from 'react-responsive'
 import Head from 'next/head'
 
 
 export default function Home() {
+      const [domLoaded, setDomLoaded] = useState(false);
 
- const scrollRef = useRef()
-  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)' })
+useEffect(() => {
+  setDomLoaded(true);
+}, []);
+      const isDesktopOrLaptop = useMediaQuery({
+            query: '(min-width: 1224px)'
+          })
+      // const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+      // const isMediumDevice = useMediaQuery(
+      //   "only screen and (min-width : 769px) and (max-width : 992px)"
+      // );
+      // const isLargeDevice = useMediaQuery({ query: '(min-width: 993px)' })
+
+      // const isLargeDevice = useMediaQuery(
+      //   "only screen and (min-width : 993px) and (max-width : 1200px)"
+      // );
+      // const isExtraLargeDevice = useMediaQuery(
+      //   "only screen and (min-width : 1201px)"
+      // );
 
   return ( 
-  
-   <div style={{backgroundColor:"black",color:"white"}} >
-      <Container fluid>
-  
-  
-     
-    <Header4 />
-    
-           {isDesktopOrLaptop ? <img src="Home.png" width="100%"/>:
-           <img src="Home-mobile-new.png" width="100%"/>}
+ 
+   <>
+   <Container fluid>
+   <Header4/>
+    {domLoaded ? <MediaQuery minWidth={1224}>
+           <img src="Home.png" width="100%"/>
            <p></p>
-           {isDesktopOrLaptop ? <img src="/AboutNew.png" width="100%"/>:
-           <img src="/About-mobile-new.png" width="100%"/>}
+           <img src="/AboutNew.png" width="100%"/>
            <p></p>
-           {isDesktopOrLaptop ? <img src="/GamiformationNew.png" width="100%"></img>:
+           <img src="/GamiformationNew.png" width="100%"></img>
+           <p></p>
+           <img src="/Business-drivers.png" width="100%"></img>
+           <p></p>
+           <img src="/TassCOMMERCE.png" width="100%"></img>
+           <p></p>
+    </MediaQuery>:''}
+    {domLoaded ? <MediaQuery maxWidth={1224}>
+           <img src="Home-mobile-new.png" width="100%"/>
+           <p></p>
+           <img src="/About-mobile-new.png" width="100%"/>
+           <p></p>
+           <img src="/Gamiformation-mobile-new.png" width="100%"></img>
+           <p></p>
+           <img src="/Business-drivers-mobile.png" width="100%"></img>
+           <p></p>
+           <img src="/TassCOMMERCE-mobile.png" width="100%"></img>
+           <p></p>
+
+    </MediaQuery>:''}
+   <img src="/Contact.png" width="100%"></img>
+           {/* {isDesktopOrLaptop ? <img src="Home.png" width="100%"/>:
+           <img src="Home-mobile-new.png" width="100%"/>
+           }
+           <p></p> */}
+          
+           {/* { isLargeDevice ? 
+            <div>
+           <img src="Home.png" width="100%"/>
+           <img src="/AboutNew.png" width="100%"/>
+           
+          
+           </div>
+          
+           :
+           <div>
+           <img src="Home-mobile-new.png" width="100%"/>
+           <img src="/About-mobile-new.png" width="100%"/>
+           
+           </div>
+           } */}
+           <p></p>
+           {/* {isDesktopOrLaptop ? <img src="/GamiformationNew.png" width="100%"></img>:
            <img src="/Gamiformation-mobile-new.png" width="100%"></img>}
            <p></p>
           
-       
            {isDesktopOrLaptop ? <img src="/Business-drivers.png" width="100%"></img>:
            <img src="/Business-drivers-mobile.png" width="100%"></img>}
            <p></p> 
@@ -42,14 +96,10 @@ export default function Home() {
            <p></p>
            <img src="/Contact.png" width="100%"></img>
            
-          
+           */}
     
-    
-    </Container>   
-   
-         
-   </div>
-  
+           </Container>
+           </>
    
   )
 }
