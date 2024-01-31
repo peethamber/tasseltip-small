34f04f3294e90be3f7d2a1927d4a2d76bb008cdd
peethamber <40764612+peethamber@users.noreply.github.com>
2024-01-31 08:20:39 +0000

    Add files via upload

--- pages/Header4.js
+++ pages/Header4.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Button, Alert, Breadcrumb, Card, Form, Row, Col, Navbar, Nav, NavDropdown, Carousel, Image, ListGroup, InputGroup} 
 from 'react-bootstrap';
 import { useMediaQuery } from 'react-responsive'
+import { motion } from 'framer-motion';
+
 export default function Header4() {
    
     const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)' })
@@ -24,7 +26,11 @@ export default function Header4() {
                
                 <Navbar.Collapse id="basic-navbar-nav">
 
-                <a href="/" style={{textDecoration:'none',color:'white'}}><h3 style={{fontFamily:"inherit",fontSize:"22px"}}>T A S S E L T I P</h3></a>
+                <motion.a initial={{opacity:0}}
+                animate={{opacity:1}}
+                transition={{duration:3}}
+                href="/" style={{textDecoration:'none',color:'white'}}><h3 style={{fontFamily:"inherit",fontSize:"22px"}}>T A S S E L T I P</h3>
+                </motion.a>
                
              
 
