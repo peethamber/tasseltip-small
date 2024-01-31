84dcc2e77abf716a3fbd30389fd728e7c53180d0
peethamber <40764612+peethamber@users.noreply.github.com>
2024-01-31 07:40:18 +0000

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,106 +1,32 @@
-import React, { useState,useEffect } from 'react';
-import { useRef } from 'react';
+import React from 'react';
+
+import ReactPlayer from 'react-player'
 import {Container, Button, Alert, Breadcrumb, Card, Form, Row, Col, Navbar, Nav, NavDropdown, Carousel, Image, ListGroup, InputGroup} 
 from 'react-bootstrap';
-
-
-import Header4 from './Header4.js'
-import { useMediaQuery } from 'react-responsive'
-import MediaQuery from 'react-responsive'
-import Head from 'next/head'
+import Header3 from './Header3';
+import Footer2 from './Footer2'
 
 
 export default function Home() {
-      const [domLoaded, setDomLoaded] = useState(false);
-
-useEffect(() => {
-  setDomLoaded(true);
-}, []);
-      const isDesktopOrLaptop = useMediaQuery({
-            query: '(min-width: 1224px)'
-          })
-      // const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
-      // const isMediumDevice = useMediaQuery(
-      //   "only screen and (min-width : 769px) and (max-width : 992px)"
-      // );
-      // const isLargeDevice = useMediaQuery({ query: '(min-width: 993px)' })
-
-      // const isLargeDevice = useMediaQuery(
-      //   "only screen and (min-width : 993px) and (max-width : 1200px)"
-      // );
-      // const isExtraLargeDevice = useMediaQuery(
-      //   "only screen and (min-width : 1201px)"
-      // );
-
+  
   return ( 
- 
-   <>
-   <Container fluid>
-   <Header4/>
-    {domLoaded ? <MediaQuery minWidth={1224}>
-           <img src="Home.png" width="100%"/>
-           
-           <img src="/AboutNew.png" width="100%"/>
-          
-           <img src="/GamiformationNew.png" width="100%"></img>
-           
-           <img src="/Business-drivers.png" width="100%"></img>
-         
-           <img src="/TassCOMMERCE.png" width="100%"></img>
-           
-    </MediaQuery>:''}
-    {domLoaded ? <MediaQuery maxWidth={1224}>
-           <img src="Home-mobile-new.png" width="100%"/>
-          
-           <img src="/About-mobile-new.png" width="100%"/>
-           
-           <img src="/Gamiformation-mobile-new.png" width="100%"></img>
-         
-           <img src="/Business-drivers-mobile.png" width="100%"></img>
-        
-           <img src="/TassCOMMERCE-mobile.png" width="100%"></img>
-          
-
-    </MediaQuery>:''}
-   <img src="/Contact.png" width="100%"></img>
-           {/* {isDesktopOrLaptop ? <img src="Home.png" width="100%"/>:
-           <img src="Home-mobile-new.png" width="100%"/>
-           }
-           <p></p> */}
-          
-           {/* { isLargeDevice ? 
-            <div>
-           <img src="Home.png" width="100%"/>
-           <img src="/AboutNew.png" width="100%"/>
-           
-          
-           </div>
-          
-           :
-           <div>
-           <img src="Home-mobile-new.png" width="100%"/>
-           <img src="/About-mobile-new.png" width="100%"/>
-           
-           </div>
-           } */}
-           <p></p>
-           {/* {isDesktopOrLaptop ? <img src="/GamiformationNew.png" width="100%"></img>:
-           <img src="/Gamiformation-mobile-new.png" width="100%"></img>}
-           <p></p>
-          
-           {isDesktopOrLaptop ? <img src="/Business-drivers.png" width="100%"></img>:
-           <img src="/Business-drivers-mobile.png" width="100%"></img>}
-           <p></p> 
-           {isDesktopOrLaptop ? <img src="/TassCOMMERCE.png" width="100%"></img>:
-           <img src="/TassCOMMERCE-mobile.png" width="100%"></img>}
-           <p></p>
-           <img src="/Contact.png" width="100%"></img>
-           
-           */}
     
-           </Container>
-           </>
+    <div style={{backgroundColor:"#f0f8ff"}}>
+    <Header3></Header3>
+    <Container fluid>
+    <p></p>
    
+             <div>
+               <Image src="/Home.png" width="100%" ></Image>
+               <Image src="/About.png" width="100%" ></Image>
+               <Image src="/Services.png" width="100%" ></Image>
+              
+               <Image src="/Contact.png" width="100%" ></Image>
+               </div>
+                 
+    
+    </Container>
+   
+   </div>
   )
 }
-
